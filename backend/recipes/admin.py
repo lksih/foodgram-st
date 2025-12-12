8dca269bc9bf042e5504937f5e7adf97486d31d5
Lksih <odindwatri123123@gmail.com>
2025-12-13 03:39:29 +0700

    Модели в админке

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import Recipe, MeasurementUnit, Ingredient, ShoppingCart, Favorited
+
+admin.site.register(Recipe, UserAdmin)
+admin.site.register(MeasurementUnit, UserAdmin)
+admin.site.register(Ingredient, UserAdmin)
+admin.site.register(ShoppingCart, UserAdmin)
+admin.site.register(Favorited, UserAdmin)
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import AvataredUser, Follow
+
+admin.site.register(AvataredUser, UserAdmin)
+admin.site.register(Follow, UserAdmin)
