4bf5c835bec36c2d229d30838e041ec7b0a477f9
Lksih <odindwatri123123@gmail.com>
2025-12-16 03:12:54 +0700

    admin.site.register

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -4,11 +4,11 @@ from .models import (
     Recipe, RecipeIngredient, Favorited, ShoppingCart
 )
 
-@admin.register(MeasurementUnit)
+
 class MeasurementUnitAdmin(admin.ModelAdmin):
     list_display = ('title', 'ingredient_count')
     search_fields = ('title',)
-    
+
     def ingredient_count(self, obj):
         return obj.ingredients.count()
     ingredient_count.short_description = 'Количество ингредиентов'
@@ -20,36 +20,38 @@ class RecipeIngredientInline(admin.TabularInline):
     min_num = 1
 
 
-@admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
     list_display = ('name', 'measurement_unit')
     search_fields = ('name',)
     list_filter = ('measurement_unit',)
 
 
-
-@admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     list_display = ('name', 'author', 'cooking_time')
     search_fields = ('name', 'author__username', 'author__email')
     inlines = [RecipeIngredientInline]
 
 
-@admin.register(Favorited)
 class FavoritedAdmin(admin.ModelAdmin):
     list_display = ('user', 'recipe', 'recipe_author')
     search_fields = ('user__username', 'recipe__name')
-    
+
     def recipe_author(self, obj):
         return obj.recipe.author
     recipe_author.short_description = 'Автор рецепта'
 
 
-@admin.register(ShoppingCart)
 class ShoppingCartAdmin(admin.ModelAdmin):
     list_display = ('user', 'recipe', 'recipe_author')
     search_fields = ('user__username', 'recipe__name')
-    
+
     def recipe_author(self, obj):
         return obj.recipe.author
     recipe_author.short_description = 'Автор рецепта'
+
+
+admin.site.register(MeasurementUnit, MeasurementUnitAdmin)
+admin.site.register(ShoppingCart, ShoppingCartAdmin)
+admin.site.register(Favorited, FavoritedAdmin)
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
