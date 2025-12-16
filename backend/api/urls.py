b65b8b101feb9325500c65605ab41958682b6cc8
Lksih <odindwatri123123@gmail.com>
2025-12-16 22:30:43 +0700

    Редирект

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from djoser.views import TokenCreateView, TokenDestroyView
+from django.views.generic import RedirectView
 
 from .views import (
     AvataredUserViewSet, RecipeViewSet, IngredientViewSet
@@ -17,4 +18,12 @@ urlpatterns = [
     # Эндпоинты Djoser для токенов
     path('auth/token/login/', TokenCreateView.as_view(), name='login'),
     path('auth/token/logout/', TokenDestroyView.as_view(), name='logout'),
+
+    # Редирект коротких ссылок
+    path('s/<int:pk>/',
+         RedirectView.as_view(
+             pattern_name='recipe-detail',
+             permanent=False
+         ),
+         name='short-link-redirect'),
 ]
--- backend/foodgram_st_backend/urls.py
+++ backend/foodgram_st_backend/urls.py
@@ -1,16 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-
-    # Редирект коротких ссылок
-    path('s/<int:pk>/',
-         RedirectView.as_view(
-             pattern_name='recipe-detail',
-             permanent=False
-         ),
-         name='short-link-redirect'),
 ]
